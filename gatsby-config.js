07d9a409b15fe78cc340faa9f0fc5f98f9adf11a
Jean-Michel Claus <jmc@math.mit.edu>
2019-05-13 11:22:07 +0200

    Add PWA support
    * gatsby-plugin-manifest@2.1.1
    * gatsby-plugin-offline@2.1.0

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,5 +11,19 @@ module.exports = {
         includePaths: ['./node_modules'],
       },
     },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      // https://developers.google.com/web/fundamentals/web-app-manifest/
+      options: {
+        name: 'MIT OpenCourseWare',         // Used in app install prompt
+        short_name: 'OCW',                  // Used in home screen or launcher
+        start_url: '/',
+        background_color: '#a31f34',        // MIT colors: http://web.mit.edu/graphicidentity/colors.html
+        theme_color: '#a31f34',
+        display: 'standalone',
+        icon: `src/images/MIT-logo.svg`,    // MIT logos: http://web.mit.edu/graphicidentity/download-logos.html
+      },
+    },
+    'gatsby-plugin-offline',
   ],
 };
