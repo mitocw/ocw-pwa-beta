486548b4d5381b877dfe6bf4438070aa36af0920
Jean-Michel Claus <jmc@math.mit.edu>
2019-05-23 19:04:17 +0200

    Use SASS maps and CSS variables to implement theming
    * Palettes are implemented using nested SASS maps
    * Names and values are extracted from them and included in :root
      as CSS variables via the extract-colors mixin
    * Added color SASS function that lets us use more easily these CSS
      variables i.e. we assign a color as follows color(name) instead of
      var(--color-name)
    * Added @material/react-button@0.12.1

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import Button from '@material/react-button';
 import styles from './header.module.scss';
 
-const Header = () => (
-  <header className={styles.header}>
-    OCW Progressive Web Application
-  </header>
-);
+const Header = () => {
+  const buttonHandleClick = useCallback(
+    () => {
+      // eslint-disable-next-line no-undef
+      const root = document.documentElement;
+      if (root.hasAttribute('theme')) {
+        root.removeAttribute('theme');
+      } else {
+        root.setAttribute('theme', 'a11y');
+      }
+    },
+  );
+
+  return (
+    <header className={styles.header}>
+      <span className={styles.title}>OCW Progressive Web Application</span>
+      <Button className={styles.button} onClick={buttonHandleClick}>Theme</Button>
+    </header>
+  );
+};
 
 export default Header;
