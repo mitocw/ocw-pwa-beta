565f486f389e8457d6bf0185f097fbebae26dbe5
Jean-Michel Claus <jmc@math.mit.edu>
2019-09-03 16:56:09 +0200

    Add some very simple tests to check previous configurations

---
+++ src/components/__tests__/simple-function.test.js
@@ -0,0 +1,5 @@
+import sum from '../simple-function';
+
+test('Adds 1 to 2', () => {
+  expect(sum(1, 2)).toBe(3);
+});
---
+++ src/components/simple-component.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SimpleComponent = () => (
+  <h1>Simple component</h1>
+);
+
+export default SimpleComponent;
