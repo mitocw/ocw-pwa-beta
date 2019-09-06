0a37ce6a56e035ad0fd8654da657648b4d4fc576
Jean-Michel Claus <jmc@math.mit.edu>
2019-09-06 10:51:51 +0200

    Enable Google Analytic via a Gatsby plugin
    * Add gatsby-plugin-google-analytics@2.1.14

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,13 +11,16 @@ const hasFullAccess = false;
 const isPreview = false;
 const hasLiveReload = false;
 
+let gaConfig;
+let gaOptions;
+
 try {
   /*
     Load the DatoCMS config from ./.datocms.json
     It must have the following structure:
     {
       "fullAccessToken": full-access-token,
-      "readOnlyAccessToken": read-only-access-token,
+      "readOnlyAccessToken": read-only-access-token
     }
   */
   // eslint-disable-next-line global-require
@@ -29,14 +32,43 @@ try {
   };
 } catch (_) {
   // eslint-disable-next-line no-console
-  console.log('Could not find datocms configuration file');
+  console.log('Could not find ./.datocms.js configuration file');
 }
 
-// Overwrite the DatoCMS config with environment variables if they exist
+try {
+  /*
+    Load the Google Analytics config from ./.google-analytics.json
+    It must have the following structure:
+    {
+      "trackingId": tracking-id
+    }
+  */
+  // eslint-disable-next-line global-require
+  gaConfig = require('./.google-analytics');
+  gaOptions = {
+    trackingId: gaConfig.trackingId,
+    // Defines where to place the tracking script - 'true' in the head and 'false' in the body
+    head: false,
+    anonymize: true,
+    // Respect “Do Not Track”
+    respectDNT: true,
+  };
+} catch (_) {
+  // eslint-disable-next-line no-console
+  console.log('Could not find ./.google-analytics.js configuration file');
+}
+
+// Overwrite the DatoCMS and Google Analytics config with environment variables if they exist
 datocmsOptions = {
   apiToken: process.env.API_TOKEN || datocmsOptions.apiToken,
   previewMode: process.env.PREVIEW_MODE || datocmsOptions.previewMode,
   disableLiveReload: process.env.DISABLE_LIVE_RELOAD || datocmsOptions.disableLiveReload,
+};
+gaOptions = {
+  trackingId: process.env.TRACKING_ID || gaOptions.trackingId,
+  head: process.env.HEAD || gaOptions.head,
+  anonymize: process.env.ANONYMIZE || gaOptions.anonymize,
+  respectDNT: process.env.RESPECT_DNT || gaOptions.respectDNT,
 };
 
 const { apiToken } = datocmsOptions;
@@ -110,6 +142,10 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-create-client-paths',
       options: { prefixes: ['/courseware/*'] },
+    },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: gaOptions,
     },
   ],
 };
