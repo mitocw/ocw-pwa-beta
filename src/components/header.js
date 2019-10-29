7a0b064bb12ed5b52b325635f23987fa87e342d7
Jean-Michel Claus <jmc@math.mit.edu>
2019-10-29 18:22:49 +0100

    Add tooltip to 'Log in' button
    * Added react-tooltip-lite@1.10.0

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,7 @@ import { navigate } from 'gatsby';
 import Button from '@material/react-button';
 import { IconContext } from 'react-icons';
 import { MdPermIdentity } from 'react-icons/md';
+import Tooltip from 'react-tooltip-lite';
 import { isAuthenticated, logout } from '../scripts/auth';
 import './header.scss';
 
@@ -33,7 +34,19 @@ const Header = () => {
       }
     },
   );
-  const logButtonText = isAuthenticated() ? 'Log out' : 'Log in';
+  const logButton = isAuthenticated()
+    ? (
+      <Button className="header-button" onClick={navigateToAccount}>
+        Log out
+      </Button>
+    )
+    : (
+      <Tooltip content="Please log in to saved courses">
+        <Button className="header-button" onClick={navigateToAccount}>
+          Log in
+        </Button>
+      </Tooltip>
+    );
   const favoriteCoursesButton = isAuthenticated()
     ? (
       <IconContext.Provider value={{ size: '1.5rem' }}>
@@ -56,9 +69,7 @@ const Header = () => {
       </div>
       <div className="header-right-items">
         {favoriteCoursesButton}
-        <Button className="header-button" onClick={navigateToAccount}>
-          {logButtonText}
-        </Button>
+        {logButton}
         <Button className="header-button" onClick={changeTheme}>
           Theme
         </Button>
