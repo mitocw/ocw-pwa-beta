48161ba589869fd531fa423eda71660f838c35a6
Jean-Michel Claus <jmc@math.mit.edu>
2019-12-19 02:24:17 +0100

    Add timeout to selects and buttons in discovery page

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,8 @@
 import { useState, useCallback } from 'react';
 import { createContainer } from 'unstated-next';
 
+const timeout = 200; // Used to let select and button change state before courseware rerendering
+
 const useStore = () => {
   const [courseSearch, setCourseSearch] = useState('');
   const [courseTopic, setCourseTopic] = useState('All');
@@ -10,22 +12,37 @@ const useStore = () => {
   const [favoriteCardType, setFavoriteCardType] = useState('condensed');
 
   const changeCourseSearch = useCallback(
-    searchTerm => setCourseSearch(searchTerm),
+    searchTerm => setTimeout(() => setCourseSearch(searchTerm), timeout),
   );
   const changeCourseTopic = useCallback(
-    event => setCourseTopic(event.detail.value),
+    event => {
+      const { value } = event.detail;
+      setTimeout(() => setCourseTopic(value), timeout);
+    },
   );
   const changeCourseFeature = useCallback(
-    event => setCourseFeature(event.detail.value),
+    event => {
+      const { value } = event.detail;
+      setTimeout(() => setCourseFeature(value), timeout);
+    },
   );
   const changeCourseLevel = useCallback(
-    event => setCourseLevel(event.detail.value),
+    event => {
+      const { value } = event.detail;
+      setTimeout(() => setCourseLevel(value), timeout);
+    },
   );
   const changeCardType = useCallback(
-    event => setCardType(event.currentTarget.dataset.cardType),
+    event => {
+      const value = event.currentTarget.dataset.cardType;
+      setTimeout(() => setCardType(value), timeout);
+    },
   );
   const changeFavoriteCardType = useCallback(
-    event => setFavoriteCardType(event.currentTarget.dataset.cardType),
+    event => {
+      const value = event.currentTarget.dataset.cardType;
+      setTimeout(() => setFavoriteCardType(value), timeout);
+    },
   );
 
   return {
