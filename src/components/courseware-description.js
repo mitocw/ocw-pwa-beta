015b5463fb7be640b1ebaa6c528b625074e9ec00
Jean-Michel Claus <jmc@math.mit.edu>
2019-12-20 23:23:30 +0100

    Start with the course description collapsible open

--- src/components/courseware-description.js
+++ src/components/courseware-description.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Collapsible from './collapsible';
 
 const CoursewareDescription = ({ className, description, url }) => (
-  <Collapsible className={className} title="Course Description">
+  <Collapsible className={className} title="Course Description" startOpen>
     <div dangerouslySetInnerHTML={{ __html: `${description}` }} />
     <a href={`https://ocw.mit.edu/${url}`}>Course Link</a>
   </Collapsible>
